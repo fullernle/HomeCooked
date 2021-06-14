84c4304db09ce36f2878650542a4c078dbefdbde
jon <jonathanyusf@gmail.com>
2021-06-14 11:45:49 -0700

    Add login functionality

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ const mongoose = require('mongoose');
 const users = require("./routes/api/users");
 const bodyParser = require('body-parser');
 
+
 const db = require('./config/keys').mongoURI;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 mongoose
   .connect(db, { 
@@ -16,8 +20,6 @@ mongoose
 
 app.get("/", (req, res) => res.send("HomeCooked!!!"));
 app.use("/api/users", users);
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
 
 const port = process.env.PORT || 5000;
