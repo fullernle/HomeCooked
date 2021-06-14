c3c3edd87a7c7229bebac12ec0ce3c6f9b380ba8
lefuller <fullernle@gmail.com>
2021-06-14 13:38:53 -0700

    add session actions and reducers

---
+++ frontend/src/components/main/main_page.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class MainPage extends Component {
+	render() {
+		return (
+			<div>
+				<h1>Welcome to HomeCooked</h1>
+
+				<footer>
+					Copyright &copy; 2021 HomeCooked
+				</footer>
+			</div>
+		)
+	}
+}
---
+++ frontend/src/components/nav/navbar.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./navbar.css";
+
+class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.logoutUser = this.logoutUser.bind(this);
+    this.getLinks = this.getLinks.bind(this);
+  }
+
+  logoutUser(e) {
+    e.preventDefault();
+    this.props.logout();
+  }
+
+  // Selectively render links dependent on whether the user is logged in
+  getLinks() {
+    if (this.props.loggedIn) {
+      return (
+        <div>
+          <Link className='nav-links' to={"/"}>Home</Link>
+          <button className='nav-bttn' onClick={this.logoutUser}>Logout</button>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <Link className='nav-links' to={"/signup"}>Signup</Link>
+          <Link className='nav-links' to={"/login"}>Login</Link>
+        </div>
+      );
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1 className='nav-logo'>HomeCooked</h1>
+        {this.getLinks()}
+      </div>
+    );
+  }
+}
+
+export default NavBar;
