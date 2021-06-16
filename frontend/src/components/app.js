0bdf633338d807a749b452c1a84e19348582dda9
ibrahim-ali00 <ibrahim.ali00@protonmail.com>
2021-06-15 18:02:32 -0700

    create mapBox functionality

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -2,16 +2,19 @@ import React from "react";
 import { AuthRoute, ProtectedRoute } from "../util/route_util";
 import { Switch, Route } from "react-router-dom";
 import NavBarContainer from "./nav/navbar_container";
-
+import MapBox from "./map/map_container";
 import MainPageContainer from "./main/main_page_container";
 import LoginFormContainer from "./session/login_form_container";
 import SignupFormContainer from "./session/signup_form_container";
 
 const App = () => (
   <div>
-		<nav>
-   		<NavBarContainer />
-		</nav>
+    <nav>
+      <NavBarContainer />
+    </nav>
+    <div>
+      <MapBox />
+    </div>
     <Switch>
       <AuthRoute exact path="/login" component={LoginFormContainer} />
       <AuthRoute exact path="/signup" component={SignupFormContainer} />
