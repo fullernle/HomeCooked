b9a0d0c2ba85036262d221ee25df250a50de1c6e
ibrahim-ali00 <ibrahim.ali00@protonmail.com>
2021-06-18 10:15:46 -0700

    flesh out businesses display

--- frontend/src/components/business/Businesses.jsx
+++ frontend/src/components/business/Businesses.jsx
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom";
 export default class Business extends Component {
   componentDidMount() {
     this.props.fetchBusinesses();
+    this.props.fetchProducts();
   }
   render() {
     if (!Array.isArray(this.props.businesses)) {
@@ -15,9 +16,21 @@ export default class Business extends Component {
           <div className={styles.List}>
             {this.props.businesses.map((business) => {
               return (
-                <Link className={styles.NavLink} to={`/homecook/${business._id}`}>
-                  {business.name}
-                </Link>
+                <>
+                  <Link
+                    className={styles.NavLink}
+                    to={`/homecook/${business._id}`}
+                  >
+                    {business.name}
+                  </Link>
+                  <p>
+                    {business.price} • {business.categories[0].title},{" "}
+                    {business.categories[1].title},{" "}
+                    {business.categories[2].title}{" "}
+                  </p>
+                  <p>{business.location.display_address[0]}</p>
+                  <p>{business.location.display_address[1]}</p>
+                </>
               );
             })}
           </div>
