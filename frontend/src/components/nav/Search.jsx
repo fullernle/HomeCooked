721e93f07023a5ec1798887ef4d1da2957748d49
Dennis Yu <dennisyu225@gmail.com>
2021-06-18 12:42:23 -0700

    add search bar styling

--- frontend/src/components/nav/Search.jsx
+++ frontend/src/components/nav/Search.jsx
@@ -1,20 +1,6 @@
 import React, { Component } from "react";
 import styles from "./Navbar.module.scss";
 
-// export default class Search extends Component {
-//   render() {
-//     return (
-//       <div>
-//         <form>
-//           <input className={styles.Search} placeholder="Search" type="text" />
-//         </form>
-//       </div>
-//     );
-//   }
-// }
-
-// ------------------------ testing
-
 export default class Search extends Component {
   render() {
     return (
