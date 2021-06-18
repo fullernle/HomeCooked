0487b2fb1addfc9b3dd26cf809b5d0a3705f93ee
lefuller <fullernle@gmail.com>
2021-06-18 11:32:12 -0700

    add product photo to search result

--- frontend/src/components/business/Businesses.jsx
+++ frontend/src/components/business/Businesses.jsx
@@ -6,36 +6,55 @@ export default class Business extends Component {
   componentDidMount() {
     this.props.fetchBusinesses();
     this.props.fetchProducts();
+
+    this.getImage = this.getImage.bind(this);
+  }
+
+  getImage(productId) {
+    console.log(productId);
+    return this.props.products.find((x) => x._id === productId).photos[0];
   }
   render() {
     if (!Array.isArray(this.props.businesses)) {
       return null;
     } else {
-      return (
-        <div className={styles.Wrapper}>
-          <div className={styles.List}>
-            {this.props.businesses.map((business) => {
-              return (
-                <>
-                  <Link
-                    className={styles.NavLink}
-                    to={`/homecook/${business._id}`}
-                  >
-                    {business.name}
-                  </Link>
-                  <p>
-                    {business.price} • {business.categories[0].title},{" "}
-                    {business.categories[1].title},{" "}
-                    {business.categories[2].title}{" "}
-                  </p>
-                  <p>{business.location.display_address[0]}</p>
-                  <p>{business.location.display_address[1]}</p>
-                </>
-              );
-            })}
+      if (this.props.products.length < 1) {
+        return null;
+      } else {
+        console.log(this.props.products);
+        return (
+          <div className={styles.Wrapper}>
+            <div className={styles.List}>
+              {this.props.businesses.map((business) => {
+                return (
+                  <>
+                    <div className={styles.Item}>
+                      <div className={styles.Details}>
+                        <Link
+                          className={styles.NavLink}
+                          to={`/homecook/${business._id}`}
+                        >
+                          {business.name}
+                        </Link>
+                        <p>
+                          {business.price} • {business.categories[0].title},{" "}
+                          {business.categories[1].title},{" "}
+                          {business.categories[2].title}{" "}
+                        </p>
+                        <p>{business.location.display_address[0]}</p>
+                        <p>{business.location.display_address[1]}</p>
+                      </div>
+                      <p className={styles.PhotoWrapper}>
+                        <img className={styles.Photo} src={this.getImage(business.products[0])} alt="" />
+                      </p>
+                    </div>
+                  </>
+                );
+              })}
+            </div>
           </div>
-        </div>
-      );
+        );
+      }
     }
   }
 }
