596205f360508e0c1347244e71c36f203358739e
lefuller <fullernle@gmail.com>
2021-07-01 14:33:42 -0700

    style about page

--- frontend/src/components/about/About.jsx
+++ frontend/src/components/about/About.jsx
@@ -6,45 +6,47 @@ import dennis from "../../images/dennis_yu.jpg";
 function About() {
   return (
     <div className={styles.Wrapper}>
-      <h1>About</h1>
-      <p>
-        Have you ever been in this situation? You want Pho, but with the love
-        and care of a personal cook. You can't go to just any restaurant to get
-        that, but you don't have someone you know that can create it either. You
-        know what you do have? HomeCooked. The home food delivery service
-        created for home cooks. Not only are you supporting your local
-        community, but you're getting a great experience as well. It is a play
-        on popular food delivery services, but for home cooks!
-      </p>
+      <div className={styles.AboutWrapper}>
+        <h1>About</h1>
+        <p>
+          Have you ever been in this situation? You want Pho, but with the love
+          and care of a personal cook. You can't go to just any restaurant to
+          get that, but you don't have someone you know that can create it
+          either. You know what you do have? HomeCooked. The home food delivery
+          service created for home cooks. Not only are you supporting your local
+          community, but you're getting a great experience as well. It is a play
+          on popular food delivery services, but for home cooks!
+        </p>
 
-      <h2>Meet our Team</h2>
-      <div className={styles.Team}>
-        <div className={styles.Member}>
-          <img src={fuller} alt="Fuller Le Image" />
-          <h3>Fuller Le</h3>
-          <a href="">Github</a>
-          <a href="">LinkedIn</a>
-        </div>
+        <h2>Meet our Team</h2>
+        <div className={styles.Team}>
+          <div className={styles.Member}>
+            <img src={fuller} alt="Fuller Le Image" />
+            <h3>Fuller Le</h3>
+            <a href="">Github</a>
+            <a href="">LinkedIn</a>
+          </div>
 
-        <div className={styles.Member}>
-          <img src={dennis} alt="Dennis Yu Image" />
-          <h3>Denni Yu</h3>
-          <a href="">Github</a>
-          <a href="">LinkedIn</a>
-        </div>
+          <div className={styles.Member}>
+            <img src={dennis} alt="Dennis Yu Image" />
+            <h3>Denni Yu</h3>
+            <a href="">Github</a>
+            <a href="">LinkedIn</a>
+          </div>
 
-        <div className={styles.Member}>
-          <img src="" alt="" />
-          <h3>Jonathan Yu</h3>
-          <a href="">Github</a>
-          <a href="">LinkedIn</a>
-        </div>
+          <div className={styles.Member}>
+            <img src="" alt="" />
+            <h3>Jonathan Yu</h3>
+            <a href="">Github</a>
+            <a href="">LinkedIn</a>
+          </div>
 
-        <div className={styles.Member}>
-          <img src="" alt="" />
-          <h3>Ibrahim Ali</h3>
-          <a href="">Github</a>
-          <a href="">LinkedIn</a>
+          <div className={styles.Member}>
+            <img src="" alt="" />
+            <h3>Ibrahim Ali</h3>
+            <a href="">Github</a>
+            <a href="">LinkedIn</a>
+          </div>
         </div>
       </div>
     </div>
