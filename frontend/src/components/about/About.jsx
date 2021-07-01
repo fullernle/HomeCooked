335165db75667077bf8b8dab22b952a66a370e8a
lefuller <fullernle@gmail.com>
2021-07-01 13:44:21 -0700

    added initial about blurb

---
+++ frontend/src/components/about/About.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import styles from "./About.module.scss";
+function About() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Have you ever been in this situation? You want Pho, but with the love
+        and care of a personal cook. You can't go to just any restaurant to get
+        that, but you don't have someone you know that can create it either. You
+        know what you do have? HomeCooked. The home food delivery service
+        created for home cooks. Not only are you supporting your local
+        community, but you're getting a great experience as well. It is a play
+        on popular food delivery services, but for home cooks!
+      </p>
+
+			<div>
+
+			</div>
+    </div>
+  );
+}
+
+export default About;
