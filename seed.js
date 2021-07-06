572671f655d0de668ac73207dda75ee5c425cdbd
jon <jonathanyusf@gmail.com>
2021-07-06 14:37:52 -0700

    Continue seeding with updated review model

--- seed.js
+++ seed.js
@@ -1,6 +1,7 @@
 var seeder = require("mongoose-seed");
 
-const db = "mongodb+srv://Seed1:!seeds1@cluster0.s5ate.mongodb.net/TestSeeds?retryWrites=true&w=majority";
+const db =
+  "mongodb+srv://Seed1:!seeds1@cluster0.s5ate.mongodb.net/TestSeeds?retryWrites=true&w=majority";
 
 seeder.connect(db, function () {
   seeder.loadModels([
@@ -9,8 +10,7 @@ seeder.connect(db, function () {
     "./models/Product",
     "./models/User",
   ]);
-  seeder.clearModels(["Business", "Cart", "Product", "User"], function(){
-
+  seeder.clearModels(["Business", "Cart", "Product", "User"], function () {
     seeder.populateModels(data, function (err, done) {
       if (err) {
         return console.log("Seeding failed", err);
@@ -28,46 +28,110 @@ const data = [
     model: "User",
     documents: [
       {
-        "username": "PeterPorker",
-        "email": "SpiderHam@blarg.com",
-        "password": "test123",
+        username: "PeterPorker",
+        email: "SpiderHam@blarg.com",
+        password: "test123",
       },
       {
-        "username": "HungryHippo",
-        "email": "hungryhungry@hippo.com",
-        "password": "test123",
+        username: "HungryHippo",
+        email: "hungryhungry@hippo.com",
+        password: "test123",
       },
       {
-        "username": "ItsAlwaysLunchTime",
-        "email": "timetoeat@asdsa.com",
-        "password": "test123",
+        username: "ItsAlwaysLunchTime",
+        email: "timetoeat@asdsa.com",
+        password: "test123",
       },
       {
-        "username": "ILoveFood",
-        "email": "foodislife@eat.com",
-        "password": "test123",
+        username: "ILoveFood",
+        email: "foodislife@eat.com",
+        password: "test123",
       },
     ],
   },
-  // {
-  //   model: "Business",
-  //   documents: [
-  //     {
-  //       "name": "Pho Hoa",
-  //       "phone": "+14152836431",
-  //       "display_phone": "(415) 283-6431",
-  //       "review_count": 1124,
-  //       "categories": [{
-  //         "title": "Vietnamese",
-  //         "title": "Asian",
-  //         "title": "Pho"
-  //       }],
-  //       "rating": 4,
-  //       "location":{
-          
-  //       }
-
-  //     }
-  //   ]
-  // },
+  {
+    model: "Business",
+    documents: [
+      {
+        name: "Pho Hoa",
+        phone: "+14152836431",
+        display_phone: "(415) 283-6431",
+        review_count: 1124,
+        categories: [
+          {
+            title: "Vietnamese",
+            title: "Asian",
+            title: "Pho",
+          },
+        ],
+        rating: 4,
+        location: {
+          address1: "4717 Bay St",
+          city: "San Francisco",
+          zip_code: "94109",
+          country: "US",
+          state: "CA",
+          display_address: ["4717 Bay St", "San Francisco, CA 94109"],
+          cross_streets: "",
+        },
+        coordinates: {
+          latitude: 37.80123,
+          longitude: -122.42214,
+        },
+        // products: [],
+        // price: "$$",
+        hours_type: "REGULAR",
+        is_open_now: true,
+        reviews: [],
+        hours: [
+          {
+            open: [
+              {
+                is_overnight: false,
+                start: "1730",
+                end: "2200",
+                day: 0,
+              },
+              {
+                is_overnight: false,
+                start: "1730",
+                end: "2200",
+                day: 1,
+              },
+              {
+                is_overnight: false,
+                start: "1730",
+                end: "2200",
+                day: 2,
+              },
+              {
+                is_overnight: false,
+                start: "1730",
+                end: "2200",
+                day: 3,
+              },
+              {
+                is_overnight: false,
+                start: "1730",
+                end: "2200",
+                day: 4,
+              },
+              {
+                is_overnight: false,
+                start: "1730",
+                end: "2200",
+                day: 5,
+              },
+              {
+                is_overnight: false,
+                start: "1730",
+                end: "2200",
+                day: 6,
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
 ];
