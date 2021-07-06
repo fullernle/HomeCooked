e864c8c1fd3828f5c14534573879f068e69471ab
lefuller <fullernle@gmail.com>
2021-07-06 16:32:57 -0700

    testing mongoose-seeder

---
+++ db.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const db = require("./config/keys").dbURI;
+
+mongoose.connect(db);
+
+mongoose.connection.on("connected", function () {
+  console.log("Mongoose connected to " + db);
+  if (process.env.NODE_ENV != "production") {
+    const seeder = require("mongoose-seeder");
+    const data = require("./data.json");
+    // const User = require("./models/User");
+    // const Business = require("./models/Business");
+    // const Product = require("./models/Product");
+    // const Review = require("./models/Review");
+    // const Cart = require("./models/Cart");
+		console.log("HELLOOO");
+    seeder
+      .seed(data, { dropDatabase: false, dropCollections: true })
+      .then((dbData) => {
+        console.log("preloading Test Data");
+        console.log(dbData);
+      })
+      .catch((err) => {
+        console.log(error);
+      });
+		console.log("WHAT");
+  }
+});
