88120fa400ba845c6822d068af82fa1334800466
lefuller <fullernle@gmail.com>
2021-07-07 10:41:52 -0700

    fix timestamp

--- models/Review.js
+++ models/Review.js
@@ -1,24 +1,26 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const ReviewSchema = new Schema({
-  business: {
-    type: Schema.Types.ObjectId,
-    ref: "Business",
+const ReviewSchema = new Schema(
+  {
+    business: {
+      type: Schema.Types.ObjectId,
+      ref: "Business",
+    },
+    body: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+    },
   },
-  body: {
-    type: String,
-    required: true,
-  },
-  username: {
-    type: String,
-    required: true,
-  },
-  rating: {
-    type: Number,
-    required: true,
-  },
-	timestamps: true
-});
+  { timestamps: true }
+);
 
 module.exports = Review = mongoose.model("Review", ReviewSchema);
